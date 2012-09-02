9ecf27b0919bd40fe2a7b8dc542c7392b5a742b8
Mark Milman <mark@miyasolutions.com>
2012-09-02 19:03:30 -0400

    clean up comments

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -46,28 +46,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   end
 
-
-  #def update
-  #  super
-  #  #@user = User.find_by_email(params[:user][:email])
-  #  #
-  #  #self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-  #  #
-  #  #if resource.update_with_password(params[resource_name])
-  #  #  if is_navigational_format?
-  #  #    if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
-  #  #      flash_key = :update_needs_confirmation
-  #  #    end
-  #  #    set_flash_message :success, flash_key || :updated
-  #  #  end
-  #  #  sign_in resource_name, resource, :bypass => true
-  #  #  respond_with resource, :location => after_update_path_for(resource)
-  #  #else
-  #  #  clean_up_passwords resource
-  #  #  respond_with resource
-  #  #end
-  #end
-
   def after_update_path_for(resource)
     profile_path
   end
