a9ee178b082d168085233295f9258960a3dec637
Mark Milman <mark@miyasolutions.com>
2012-12-04 06:46:15 -0500

    event refactoring + canceled notification draft

--- app/events/service_call_cancel_event.rb
+++ app/events/service_call_cancel_event.rb
@@ -14,25 +14,28 @@
 #  reference_id   :integer
 #
 
-class ServiceCallCancelEvent < Event
+class ServiceCallCancelEvent < ServiceCallEvent
 
   def init
     self.name = I18n.t('service_call_cancel_event.name') if name.nil?
-    self.description = I18n.t('service_call_cancel_event.description') if description.nil?
+    self.description = I18n.t('service_call_cancel_event.description', user: user) if description.nil?
     self.reference_id = 9
   end
 
-  def process_event
-    Rails.logger.debug { "Running ServiceCallCancelEvent process" }
+  def notification_recipients
+    nil
+  end
 
-    service_call = associated_object
+  def notification_class
+    nil
+  end
 
+  def update_provider
     prov_service_call = ServiceCall.find_by_ref_id_and_organization_id(service_call.ref_id, service_call.provider_id)
-    prov_service_call.events << ServiceCallCanceledEvent.new(description: I18n.t('service_call_canceled_event.description', user: user.name, org: user.organization.name)) unless service_call.id == prov_service_call.id
+    prov_service_call.events << ServiceCallCanceledEvent.new
 
     prov_service_call
 
   end
-
 
 end
