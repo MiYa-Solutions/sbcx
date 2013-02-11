8a3bfb6837f1c1f99e11f49cfa1644891ac629a6
Mark Milman <mark@miyasolutions.com>
2013-02-10 23:17:47 -0500

    final touches before submission for design

--- app/models/payments/credit_card.rb
+++ app/models/payments/credit_card.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id           :integer         not null, primary key
+#  agreement_id :integer
+#  type         :string(255)
+#  rate         :float
+#  rate_type    :string(255)
+#  created_at   :datetime        not null
+#  updated_at   :datetime        not null
+#
+
 class CreditCard < Payment
   # To change this template use File | Settings | File Templates.
 end
