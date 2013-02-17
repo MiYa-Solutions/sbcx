53f8c0ec06fc37aae50740580fe4bd559aee11fe
ishayyaari <ishay@miyasolutions.com>
2013-02-17 15:52:31 -0500

    Web:  Affiliate Show

--- db/migrate/20130113015617_index_users_preferences.rb
+++ db/migrate/20130113015617_index_users_preferences.rb
@@ -1,9 +1,9 @@
 class IndexUsersPreferences < ActiveRecord::Migration
   def up
-    execute "CREATE INDEX users_preferences ON users USING GIN(preferences)"
+    #execute "CREATE INDEX users_preferences ON users USING GIN(preferences)"
   end
 
   def down
-    execute "DROP INDEX users_preferences"
+    #execute "DROP INDEX users_preferences"
   end
 end
