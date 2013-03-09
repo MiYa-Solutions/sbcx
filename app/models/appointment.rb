4bac447fcfc420771bca9e5eb4cf0dd30a19281d
Mark Milman <mark@miyasolutions.com>
2013-03-09 16:50:47 -0500

    fixed #113

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -21,26 +21,53 @@
 class Appointment < ActiveRecord::Base
   belongs_to :appointable, :polymorphic => true
   belongs_to :organization
-  validates_presence_of :starts_at, :ends_at, :title, :appointable, :organization
-  validate :start_before_end
   stampable
 
-  scope :before, ->(end_time) { {:conditions => ["ends_at < ?", Appointment.format_date(end_time)] }}
-  scope :after, ->(start_time) { {:conditions => ["starts_at > ?", Appointment.format_date(start_time)] }}
-  scope :my_appointments, ->(org_id, start_time, end_time) { where("organization_id = ?", org_id).before(end_time).after(start_time)}
+  validates_presence_of :title, :appointable, :organization
+
+  validate :check_starts_at_text
+  validate :check_ends_at_text
+  validate :start_before_end
+
+  scope :before, ->(end_time) { { :conditions => ["ends_at < ?", Appointment.format_date(end_time)] } }
+  scope :after, ->(start_time) { { :conditions => ["starts_at > ?", Appointment.format_date(start_time)] } }
+  scope :my_appointments, ->(org_id, start_time, end_time) { where("organization_id = ?", org_id).before(end_time).after(start_time) }
+
+  # virtual attributes
+  attr_writer :starts_at_date_text, :starts_at_time_text, :ends_at_date_text, :ends_at_time_text
+
+  # transform the dates before saving
+  before_save :save_starts_at_text
+  before_save :save_ends_at_text
+
+  def starts_at_date_text
+    @starts_at_date_text || (I18n.localize(Time.zone.parse(starts_at.to_s), format: :date_only ) if starts_at.present?)
+  end
+
+  def starts_at_time_text
+    @starts_at_time_text || (I18n.localize(starts_at.to_time.in_time_zone(Time.zone), format: :time_only) if starts_at.present?)
+  end
+
+  def ends_at_date_text
+    @ends_at_date_text || (I18n.localize(Time.zone.parse(ends_at.to_s), format: :date_only) if ends_at.present?)
+  end
+
+  def ends_at_time_text
+    @ends_at_time_text || (I18n.localize(ends_at.to_time.in_time_zone(Time.zone), format: :time_only) if ends_at.present?)
+  end
 
 
   def as_json(options = {})
     {
-        :id => self.id,
-        :title => self.title,
-        :description => self.description || "",
-        :start => starts_at,
-        :end => ends_at,
-        :allDay => all_day,
-        :recurring => recurring,
+        :id               => self.id,
+        :title            => self.title,
+        :description      => self.description || "",
+        :start            => starts_at,
+        :end              => ends_at,
+        :allDay           => all_day,
+        :recurring        => recurring,
         #:eurl => Rails.application.routes.url_helpers.appointment_path(id),
-        :appointable_id => appointable_id,
+        :appointable_id   => appointable_id,
         :appointable_type => appointable_type
 
     }
@@ -53,9 +80,32 @@ class Appointment < ActiveRecord::Base
   end
 
   private
-  def start_before_end
-    errors.add(:ends_at, "Put error text here") if ends_at && ends_at <= starts_at
+
+  def check_ends_at_text
+    errors.add :ends_at_date_text, "cannot be blank" unless @ends_at_date_text.present? || ends_at.present?
+    errors.add :ends_at_date_text, "cannot be parsed" if @ends_at_date_text.present? && Time.zone.parse(@ends_at_date_text).nil?
+  rescue ArgumentError
+    errors.add :ends_at_date_text, "is out of range"
   end
 
+  def check_starts_at_text
+    errors.add :starts_at_date_text, "Can't be blank"  unless @starts_at_date_text.present? || starts_at.present?
+    errors.add :starts_at_date_text, "cannot be parsed" if @starts_at_date_text.present? && Time.zone.parse(@starts_at_date_text).nil?
+  rescue ArgumentError
+    errors.add :starts_at_date_text, "is out of range"
+  end
+
+  def start_before_end
+    errors.add(:ends_at_date_text, "End time can't be before start time") if ends_at && ends_at <= starts_at
+    errors.add(:ends_at_date_text, "End time can't be before start time") if ends_at.nil? && starts_at.nil? && Time.zone.parse(@ends_at_date_text) && Time.zone.parse("#{@ends_at_date_text} #{@ends_at_time_text}") <= Time.zone.parse("#{@starts_at_date_text} #{@starts_at_time_text}")
+  end
+
+  def save_starts_at_text
+    self.starts_at = Time.zone.parse("#{@starts_at_date_text} #{@starts_at_time_text}") if @starts_at_date_text.present?
+  end
+
+  def save_ends_at_text
+    self.ends_at = Time.zone.parse("#{@ends_at_date_text} #{@ends_at_time_text}") if @ends_at_date_text.present?
+  end
 
 end
