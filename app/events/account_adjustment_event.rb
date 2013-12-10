9781ed980ce52ec35f8c07dbe5824bf86b15b2dc
Mark Milman <mark@miyasolutions.com>
2013-12-10 10:14:33 -0500

    account adjustment event is done and green

---
+++ app/events/account_adjustment_event.rb
@@ -0,0 +1,28 @@
+require 'hstore_setup_methods'
+class AccountAdjustmentEvent < Event
+  extend HstoreSetupMethods
+
+  setup_hstore_attr :entry_id
+
+  def init
+    self.name         = I18n.t('account_adjustment_event.name')
+    self.description  = I18n.t('account_adjustment_event.description')
+    self.reference_id = 300000
+  end
+
+  def process_event
+    affiliate_account.events <<
+        AccountAdjustedEvent.new(triggering_event: self) if affiliate.subcontrax_member?
+  end
+
+  private
+
+  def affiliate_account
+    Account.for_affiliate(eventable.organization, eventable.accountable).first
+  end
+
+  def affiliate
+    eventable.accountable
+  end
+
+end
---
+++ spec/events/account_adjustment_event_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe AccountAdjustmentEvent do
+
+  let(:org) { mock_model(Organization, id: 1, name: 'Test Org1', providers: [], subcontractors: []) }
+  let(:org2) { mock_model(Organization, id: 2, name: 'Test Org2', providers: [], subcontractors: []) }
+  let(:acc) { mock_model(Account, id: 1, name: 'Test Org', organization: org, accountable: org2, events: []) }
+  let(:entry) { mock_model(MyAdjEntry, id: 1, save: true) }
+  let(:event) { AccountAdjustmentEvent.new(eventable: acc, entry_id: entry.id) }
+
+
+  context 'when created' do
+
+    before do
+      Account.stub(:for_affiliate => [acc])
+    end
+
+    context 'when the affiliate is a member' do
+      before do
+        org2.stub(:subcontrax_member? => true)
+      end
+      it 'should create AccountAdjustedEvent' do
+        AccountAdjustedEvent.should_receive(:new)
+        event.save
+      end
+
+      it 'should save the entry id in the properties' do
+        event.save
+        expect(event.properties).to eq({ 'entry_id' => '1' })
+      end
+
+    end
+    context 'when the affiliate is NOT a member' do
+      before do
+        org2.stub(:subcontrax_member? => false)
+      end
+      it 'should NOT create AccountAdjustedEvent' do
+        AccountAdjustedEvent.should_not_receive(:new)
+        event.save
+      end
+
+      it 'should save the entry id in the properties' do
+        event.save
+        expect(event.properties).to eq({ 'entry_id' => '1' })
+      end
+
+    end
+
+  end
+
+
+  context 'validation' do
+
+    before do
+      event.valid?
+    end
+
+    it 'the event should be valid' do
+      expect(event).to be_valid
+    end
+
+    it 'should have a reference id of 2' do
+      expect(event.reference_id).to be 300000
+    end
+
+    it 'should have a description taken from I18n' do
+      expect(event.description).to eq I18n.t('account_adjustment_event.description')
+    end
+
+    it 'should have a description taken from I18n' do
+      expect(event.name).to eq I18n.t('account_adjustment_event.name')
+    end
+  end
+end
