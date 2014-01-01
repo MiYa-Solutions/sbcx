57d388237d40eeefc2494334a0e4655cc2c58808
Mark Milman <mark@miyasolutions.com>
2014-01-01 11:09:11 -0500

    fixed #402

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -177,6 +177,8 @@ authorization do
     end
 
     has_permission_on :posting_rules, :to => [:new, :update, :edit, :create, :destroy] do
+
+      # The reason the below is commented - when the agreement is active DONT allow the changing of the rules
       #if_attribute :agreement => { :type => is_in { ['SubcontractingAgreement', 'OrganizationAgreement'] }, :status => is { OrganizationAgreement::STATUS_ACTIVE }, :counterparty_id => is { user.organization_id } }
       #if_attribute :agreement => { :type => is_in { ['SubcontractingAgreement', 'OrganizationAgreement'] }, :status => is { OrganizationAgreement::STATUS_ACTIVE }, :organization_id => is { user.organization_id } }
 
