39816d50f6694157ca4753b47d0797e1281267ae
Mark Milman <mark@miyasolutions.com>
2014-03-30 13:08:24 -0400

    partially collected integration tests is green

--- spec/integration/provider/customer_billing/prov_with_transfer_to_member_customer_billing_spec.rb
+++ spec/integration/provider/customer_billing/prov_with_transfer_to_member_customer_billing_spec.rb
@@ -226,7 +226,7 @@ describe 'Customer Billing When Provider Transfers To Member' do
           subcon_job.reload
         end
 
-        # bug in transferred service call #payment_entries (when it is a mixed entry it doesn't go to collected)
+                                            # bug in transferred service call #payment_entries (when it is a mixed entry it doesn't go to collected)
         let(:collection_job) { subcon_job }
         let(:collector) { subcon }
         let(:billing_status) { :na }        # since the job is not done it is set to partial
@@ -238,7 +238,7 @@ describe 'Customer Billing When Provider Transfers To Member' do
 
         let(:customer_balance_before_payment) { 990 }
         let(:payment_amount) { 990 }
-        let(:job_events) { [ScCollectEvent, ScProviderCollectedEvent, ServiceCallAcceptEvent, ServiceCallCompleteEvent, ServiceCallReceivedEvent, ServiceCallStartEvent ] }
+        let(:job_events) { [ScCollectEvent, ScProviderCollectedEvent, ServiceCallAcceptEvent, ServiceCallCompleteEvent, ServiceCallReceivedEvent, ServiceCallStartEvent] }
         let(:the_prov_job) { job.reload }
         let(:the_billing_status) { :collected }
         let(:the_subcon_collection_status) { :collected }
@@ -269,7 +269,63 @@ describe 'Customer Billing When Provider Transfers To Member' do
     describe 'when payment is late' do
       include_context 'when late'
     end
-    describe 'collect'
+
+    describe 'collect' do
+
+      context 'when collecting partial amount' do
+        let(:collection_job) { job }
+        let(:collector) { org }
+        let(:billing_status) { :partially_collected }        # since the job is not done it is set to partial
+        let(:billing_status_4_cash) { :partially_collected } # since the job is not done it is set to partial
+        let(:subcon_collection_status) { :pending }
+        let(:subcon_collection_status_4_cash) { :pending }
+        let(:prov_collection_status) { nil }
+        let(:prov_collection_status_4_cash) { nil }
+
+        let(:customer_balance_before_payment) { -100 }
+        let(:payment_amount) { 10 }
+        let(:job_events) { [ScCollectEvent, ScCollectEvent, ServiceCallTransferEvent] }
+        let(:the_prov_job) { nil }
+        let(:the_billing_status) { nil }
+        let(:the_subcon_collection_status) { nil }
+
+        include_examples 'successful customer payment collection'
+
+      end
+
+      context 'when collecting the full amount' do
+
+        describe 'after the job is done' do
+          before do
+            complete_the_work subcon_job
+          end
+          let(:collection_job) { subcon_job }
+          let(:collector) { subcon }
+          let(:billing_status) { :na }        # since the job is not done it is set to partial
+          let(:billing_status_4_cash) { :na } # since the job is not done it is set to partial
+          let(:subcon_collection_status) { nil }
+          let(:subcon_collection_status_4_cash) { nil }
+          let(:prov_collection_status) { :collected }
+          let(:prov_collection_status_4_cash) { :collected }
+
+          let(:customer_balance_before_payment) { 900 }
+          let(:payment_amount) { 900 }
+          let(:job_events) { [ScCollectEvent, ScCollectEvent, ServiceCallAcceptEvent, ServiceCallCompleteEvent, ServiceCallReceivedEvent, ServiceCallStartEvent] }
+          let(:the_prov_job) { job.reload }
+          let(:the_billing_status) { :collected }
+          let(:the_subcon_collection_status) { :collected }
+
+          include_examples 'successful customer payment collection'
+
+          describe 'job billing status' do
+            include_context 'after collecting the full amount'
+          end
+        end
+
+
+      end
+
+    end
   end
 
   context 'when in overdue state' do
@@ -449,9 +505,9 @@ describe 'Customer Billing When Provider Transfers To Member' do
       before do
         job.reload.reimburse_payment!
       end
-       it 'billing status should be paid' do
-         expect(job.billing_status_name).to eq :paid
-       end
+      it 'billing status should be paid' do
+        expect(job.billing_status_name).to eq :paid
+      end
 
     end
 
