96059c465585ce9ae8a55b2cbee531940dff1aca
Mark Milman <mark@miyasolutions.com>
2014-04-07 16:14:29 -0400

    when billing in collected state green, but with pending tests to write

--- spec/integration/customer_billing/when_transfer_to_member/after_subcon_accepts/04_after_subcon_completes_the_job_spec.rb
+++ spec/integration/customer_billing/when_transfer_to_member/after_subcon_accepts/04_after_subcon_completes_the_job_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Subcon Collection After Customer accepts' do
+describe 'Subcon Collection After Subcon accepts' do
 
   include_context 'transferred job'
 
@@ -11,18 +11,11 @@ describe 'Subcon Collection After Customer accepts' do
     add_bom_to_job subcon_job, cost: '100', price: '1000', quantity: '1'
   end
 
-  context 'when the job is accepted by the subcon' do
-
-    before do
-      accept_the_job subcon_job
-    end
 
     it 'should  allow a collection for the subcon and provider' do
       expect(subcon_job.billing_status_events).to eq [:collect]
       expect(job.reload.billing_status_events).to include(:collect)
     end
-
-  end
 
 
 end
