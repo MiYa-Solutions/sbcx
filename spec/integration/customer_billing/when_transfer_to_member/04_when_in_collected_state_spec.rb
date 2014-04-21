3c310b7c03ccc2b7a7579c6a83897215829698f7
Mark Milman <mark@miyasolutions.com>
2014-04-21 07:48:02 -0400

    billing status tests collected is green and done

--- spec/integration/customer_billing/when_transfer_to_member/04_when_in_collected_state_spec.rb
+++ spec/integration/customer_billing/when_transfer_to_member/04_when_in_collected_state_spec.rb
@@ -10,6 +10,7 @@ describe 'Billing when in collected state' do
     start_the_job subcon_job
     add_bom_to_job subcon_job, cost: 100, price: 1000, quantity: 1
     complete_the_work subcon_job
+    job.reload
   end
 
 
@@ -113,9 +114,7 @@ describe 'Billing when in collected state' do
       end
 
 
-
     end
-
 
     context 'when all cash and overpaid' do
 
@@ -163,9 +162,172 @@ describe 'Billing when in collected state' do
 
   end
 
-  pending 'when collected by provider'
+  context 'when collected by provider' do
 
-  pending 'when both provider and subcon collected'
+    context 'when collecting only cash' do
+      before do
+        collect_a_payment job, type: 'cash', amount: 1000
+      end
+
+      it 'subcon balance should match provider balance' do
+        expect(job.subcon_balance + subcon_job.provider_balance).to eq Money.new(0)
+      end
+
+      it 'billing status should be collected' do
+        expect(job.reload.billing_status_name).to eq :collected
+      end
+
+      it 'billing events should be :deposited' do
+        expect(job.reload.billing_status_events.sort).to eq [:deposited]
+      end
+
+      it 'deposited should not be allowed for user' do
+        expect(event_permitted_for_job?('billing_status', 'deposited', user, job)).to be_false
+      end
+
+
+      it 'the corresponding payment should allow to deposit clear and reject as this was collected but the provider' do
+        expect(job.reload.payments.last.allowed_status_events.sort).to eq [:clear, :deposit, :reject]
+      end
+
+      it 'billing status should be collected' do
+        expect(job.reload.billing_status_name).to eq :collected
+      end
+
+      context 'when depositing customer payments' do
+        before do
+          deposit_all_entries job.payments
+          job.reload
+        end
+
+        it 'billing status should be paid' do
+          expect(job.billing_status_name).to eq :paid
+        end
+
+      end
+
+
+    end
+
+    context 'when collecting mixed cash and credit' do
+
+      before do
+        collect_a_payment subcon_job, type: 'cash', amount: 500
+        collect_a_payment subcon_job, type: 'credit_card', amount: 500
+      end
+      it 'subcon balance should match provider balance' do
+        expect(job.subcon_balance + subcon_job.provider_balance).to eq Money.new(0)
+      end
+
+      it 'billing status should be collected' do
+        expect(job.reload.billing_status_name).to eq :collected
+      end
+
+      context 'when fully deposited to provider' do
+        before do
+          deposit_all_entries subcon_job.collection_entries
+        end
+
+        it 'the corresponding payment should now allow to deposit clear and reject as the subcon marked collection as deposited' do
+          expect(job.reload.payments.last.allowed_status_events.sort).to eq [:clear, :deposit, :reject]
+        end
+
+        it 'billing status should be in_process' do
+          expect(job.reload.billing_status_name).to eq :collected
+        end
+
+        context 'when depositing customer payments' do
+          before do
+            deposit_all_entries job.payments
+            job.reload
+          end
+
+          it 'billing status should be in process' do
+            expect(job.billing_status_name).to eq :in_process
+          end
+
+        end
+
+
+      end
+
+
+    end
+
+    context 'when all cash and overpaid' do
+
+      before do
+        collect_a_payment subcon_job, type: 'cash', amount: 1100
+      end
+
+      it 'subcon balance should match provider balance' do
+        expect(job.subcon_balance + subcon_job.provider_balance).to eq Money.new(0)
+      end
+
+      it 'billing status should be collected' do
+        expect(job.reload.billing_status_name).to eq :collected
+      end
+
+      context 'when fully deposited to provider' do
+        before do
+          deposit_all_entries subcon_job.collection_entries
+        end
+
+        it 'the corresponding payment should now allow to deposit clear and reject as the subcon marked collection as deposited' do
+          expect(job.reload.payments.last.allowed_status_events.sort).to eq [:clear, :deposit, :reject]
+        end
+
+        it 'billing status should be in_process' do
+          expect(job.reload.billing_status_name).to eq :collected
+        end
+
+        context 'when depositing customer payments' do
+          before do
+            deposit_all_entries job.payments
+            job.reload
+          end
+
+          it 'billing status should be paid' do
+            expect(job.billing_status_name).to eq :over_paid
+          end
+
+        end
+
+
+      end
+
+    end
+
+  end
+
+  context 'when both provider and subcon collected' do
+    context 'when collecting cash' do
+      before do
+        collect_a_payment job, type: 'cash', amount: 500
+        collect_a_payment subcon_job, type: 'cash', amount: 500
+        job.reload
+        subcon_job.reload
+      end
+
+      it 'subcon balance should match provider balance' do
+        expect(job.subcon_balance + subcon_job.provider_balance).to eq Money.new(0)
+      end
+
+      it 'billing status should be collected' do
+        expect(job.billing_status_name).to eq :collected
+      end
+
+      it 'the provider collected payment should allow to deposit clear and reject ' do
+        expect(job.payments.first.allowed_status_events.sort).to eq [:clear, :deposit, :reject]
+      end
+
+      it 'the subcon collected payment should not allow to deposit clear and reject ' do
+        expect(job.payments.last.allowed_status_events.sort).to eq []
+      end
+
+
+    end
+  end
 
 
 end
