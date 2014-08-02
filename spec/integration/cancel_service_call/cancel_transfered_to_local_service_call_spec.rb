5a664b82121b10a74d852c3478d3995193667177
Mark Milman <mark@miyasolutions.com>
2014-08-02 11:06:02 -0400

    #429 - cancel revised to account for new billing for a transferred to local

--- spec/integration/cancel_service_call/cancel_transfered_to_local_service_call_spec.rb
+++ spec/integration/cancel_service_call/cancel_transfered_to_local_service_call_spec.rb
@@ -56,12 +56,9 @@ describe 'Cancel Job Transferred To Local' do
     end
 
     it 'the customer should be reimbursed' do
-      expect(job.entries.where(type: 'CanceledJobAdjustment').sum(:amount_cents)).to eq -100000
+      expect(job.entries.where(type: 'CanceledJobAdjustment', account_id: job.customer.account.id).sum(:amount_cents)).to eq -100000
     end
 
-    it 'subcon balance should be -100.00 (reimbursement for the part)' do
-      expect(job.organization.account_for(subcon).balance).to eq Money.new(-10000)
-    end
 
   end
 
@@ -69,6 +66,7 @@ describe 'Cancel Job Transferred To Local' do
     before do
       collect_a_payment job, amount: 100, type: 'cash', collector: subcon
       cancel_the_job job
+      job.reload
     end
 
     it 'status should be changed to canceled' do
@@ -76,22 +74,23 @@ describe 'Cancel Job Transferred To Local' do
     end
 
     it 'billing status should be over paid' do
-      expect(job.billing_status_name).to eq :overpaid
+      expect(job.billing_status_name).to eq :collected
     end
 
-    it 'subcon balance should be 100.00 ' do
+    it 'subcon balance should be 100.00' do
       expect(job.organization.account_for(subcon).balance).to eq Money.new(10000)
     end
   end
 
-  describe 'when a payment was fully collected by the subcon and job completed' do
+  describe 'when a payment was fully collected part by the subcon and part by the prov (job completed)' do
     before do
-      collect_a_payment job, amount: 100, type: 'cash', collector: subcon
+      collect_a_payment job, amount: 100, type: 'cash', collector: org
       start_the_job job
       add_bom_to_job job, cost: 200, price: 1000, quantity: 1, buyer: subcon
-      collect_a_payment job, amount: 100, type: 'cheque', collector: org
+      collect_a_payment job, amount: 900, type: 'cheque', collector: subcon
       complete_the_work job
       cancel_the_job job
+      job.reload
     end
 
     it 'status should be changed to canceled' do
@@ -99,21 +98,57 @@ describe 'Cancel Job Transferred To Local' do
     end
 
     it 'billing status should be over paid' do
-      expect(job.billing_status_name).to eq :overpaid
+      expect(job.billing_status_name).to eq :collected
     end
 
-    it 'subcon balance should be -100.00 (reimbursement for the part - collection)' do
-      expect(job.organization.account_for(subcon).balance).to eq Money.new(-10000)
+    it 'subcon balance should be 900.00 - the part reimbursement is reversed' do
+      expect(job.organization.account_for(subcon).balance).to eq Money.new(90000)
     end
 
-
-    context 'when reimbursing the customer' do
+    context 'when depositing on behalf of the subcon' do
       before do
-        job.reimburse_payment!
+        job.collected_entries.last.deposited!
+        job.reload
+      end
+      it 'billing events should be :deposited' do
+        expect(job.billing_status_events.sort).to eq [:deposited]
       end
 
-      it 'status should changed to reimbursed' do
-        expect(job.billing_status_name).to eq :reimbursed
+      context 'when depositing all payments' do
+        before do
+          deposit_all_entries job.payments
+          job.reload
+        end
+
+        it 'billing events should be :clear' do
+          expect(job.billing_status_events.sort).to eq [:clear]
+        end
+
+        context 'when rejecting the check' do
+          before do
+            job.payments.last.reject!
+            job.reload
+          end
+
+          it 'billing events should be :reimburse' do
+            expect(job.billing_status_events.sort).to eq [:reimburse]
+          end
+
+          it 'billing status should be over_paid' do
+            expect(job.billing_status_name).to eq :over_paid
+          end
+
+
+          context 'when reimbursing the customer' do
+            before do
+              job.reimburse_payment!
+            end
+
+            it 'status should changed to paid' do
+              expect(job.billing_status_name).to eq :paid
+            end
+          end
+        end
       end
     end
   end
@@ -124,29 +159,52 @@ describe 'Cancel Job Transferred To Local' do
       start_the_job job
       add_bom_to_job job, cost: 100, price: 1000, quantity: 1, buyer: subcon
       complete_the_work job
-      cancel_the_job job
+      job.reload
     end
 
-    it 'status should be changed to canceled' do
-      expect(job.status_name).to eq :canceled
-    end
-
-    it 'billing status should be over paid' do
-      expect(job.billing_status_name).to eq :overpaid
-    end
-
-    it 'subcon balance should be -100.00 (reimbursement for the part - collection)' do
-      expect(job.organization.account_for(subcon).balance).to eq Money.new(-10000)
+    it 'subcon balance should be 2.00 (reimbursement for the part + job fee + payment fee)' do
+      expect(job.organization.account_for(subcon).balance).to eq Money.new(-10000 -10000 +20000 + 200)
     end
 
 
-    context 'when reimbursing the customer' do
+    context 'when canceling the job' do
+
       before do
-        job.reimburse_payment!
+        cancel_the_job job
+        job.reload
+      end
+      it 'status should be changed to canceled' do
+        expect(job.status_name).to eq :canceled
       end
 
-      it 'status should changed to reimbursed' do
-        expect(job.billing_status_name).to eq :reimbursed
+      it 'billing status should be over paid' do
+        expect(job.billing_status_name).to eq :collected
+      end
+
+      it 'subcon balance should be 200 (no reimbursement for the part nor payment fee)' do
+        expect(job.organization.account_for(subcon).balance).to eq Money.new(20000)
+      end
+
+
+      describe 'when depositing the payment' do
+        before do
+          job.payments.last.deposit!
+          job.reload
+        end
+
+        it 'billing status should be over_paid' do
+          expect(job.billing_status_name).to eq :over_paid
+        end
+
+        context 'when reimbursing the customer' do
+          before do
+            job.reimburse_payment!
+          end
+
+          it 'status should changed to paid' do
+            expect(job.billing_status_name).to eq :paid
+          end
+        end
       end
     end
 
