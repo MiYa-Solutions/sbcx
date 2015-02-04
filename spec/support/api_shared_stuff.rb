72958878528ee3a7ec015c7ad420d88aaba465c7
Mark Milman <mark@miyasolutions.com>
2015-02-04 18:29:55 -0500

    fixed testing problems

--- spec/support/api_shared_stuff.rb
+++ spec/support/api_shared_stuff.rb
@@ -1,7 +1,7 @@
 shared_context 'api' do
   def api_sign_in(user, options = {})
     post 'api/v1/sign_in',
-         { user: { email: user.email, password: user.password }, format: :json },
+         { user: { email: user.email, password: 'SbcxTest123' }, format: :json },
          { 'Content-Type' => 'application/json',
            'Accept'       => 'application/json' }
   end
